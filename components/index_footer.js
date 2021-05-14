c5c81be1b3b8204425f441e5b215510666428812
harshalpro224 <harshalbhagat224@gmail.com>
2021-05-14 12:07:05 +0530

    add website

--- components/index_footer.js
+++ components/index_footer.js
@@ -6,42 +6,27 @@ class Footer extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <!-- Footer -->
-	<footer class="page-footer bg-dark" >
+	<footer class="page-footer bg-dark" style="margin-top: 20px;">
 		<div class="container">
-			<div class="container-sm">
-				<div class="row">
-					<div class="col-sm-4">
-						<h5 class="font-weight-bold text-uppercase mt-3 mb-4" style="color: white;">Blank Page</h5>
-					</div>
-					<div class="col-sm-4">
-						<!-- Links -->
-				        <h5 class="font-weight-bold mt-3 mb-4" style="color: white;">Main Link</h5>
-
-				        <ul class="list-unstyled">
-				          <li>
-				            <a href="#!" id="style-link">Contact Us</a>
-				          </li>
-				          <li>
-				            <a href="#!" id="style-link">Privacy Policy</a>
-				          </li>
-				        </ul>
-					</div>
-					<div class="col-sm-4" style="margin-bottom: 20px;">
-						<h5 class="font-weight-bold mt-3 mb-4" style="color: white;">Social Media</h5>
-
-						<!-- Social buttons -->
-						<a href="#" class="fa fa-facebook"></a>
-						<a href="#" class="fa fa-instagram"></a>
-						<a href="#" class="fa fa-twitter"></a>
+			<div class="row">
+				<div class="col-4"></div>
+				<div class="col-4">
+					<div id="contact">
+						<center>
+							<a href="https://www.instagram.com/programmers0_0/"><img src="../assets/social/instagram.png" id="contact-logo"></a>
+							<a href="https://www.facebook.com/harshal1882001/"><img src="../assets/social/facebook.png" id="contact-logo"></a>
+							<a href="https://twitter.com/programmers0_0"><img src="../assets/social/twitter.png" id="contact-logo"></a>
+						</center>
 					</div>
 				</div>
+				<div class="col-4"></div>
 			</div>
-		</div>
-		<!-- Copyright -->
 		  <div class="footer-copyright text-center py-3" style="color: white;">©2021 Copyright:
-		    <a href="">@devopscoding</a>
+		    <a href="https://www.instagram.com/programmers0_0/">@programmers0_0</a>
 		  </div>
-		</footer>
+		</div>
+		</div>
+	</footer>
     `;
   }
 }
